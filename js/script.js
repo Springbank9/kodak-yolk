a89c1e2cb64d9a6c11ac9e2eeb1d360ed356568d
Springbank9 <isabellahay7@gmail.com>
2021-06-14 13:41:29 +0000

    added background image and navbar

--- js/script.js
+++ js/script.js
@@ -1,73 +1,59 @@
-$(function () {
 
-    // init feather icons
-    feather.replace();
+/*global $ */
+$(document).ready(function() {
 
-    // init tooltip & popovers
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="popover"]').popover();
+  "use strict";
 
-    //page scroll
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 20
-        }, 1000);
-        event.preventDefault();
+  $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+  //Checks if li has sub (ul) and adds class for toggle icon - just an UI
+
+  $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+  //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
+
+  $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">Navigation</a>");
+
+  //Adds menu-mobile class (for mobile toggle menu) before the normal menu
+  //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
+  //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
+  //Done this way so it can be used with wordpress without any trouble
+
+  $(".menu > ul > li").hover(function(e) {
+    if ($(window).width() > 943) {
+      $(this).children("ul").stop(true, false).fadeToggle(150);
+      e.preventDefault();
+    }
+  });
+  //If width is more than 943px dropdowns are displayed on hover
+
+  $(".menu > ul > li").click(function() {
+    if ($(window).width() <= 943) {
+      $(this).children("ul").fadeToggle(150);
+    }
+  });
+  //If width is less or equal to 943px dropdowns are displayed on click (thanks Aman Jain from stackoverflow)
+
+  $(".menu-mobile").click(function(e) {
+    $(".menu > ul").toggleClass('show-on-mobile');
+    e.preventDefault();
+  });
+  //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
+
+});
+
+
+
+
+$(document).ready(function(){
+  
+  setTimeout(function(){
+    
+    $("#name").removeClass("larger");
+
+    $("#name span").each(function(){
+      $(this).css("-webkit-transition-delay",$(this).data("delay")+"ms").css("transition-delay",$(this).data("delay")+"ms");
+      $(this).addClass("visible");
     });
-
-    // slick slider
-    $('.slick-about').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        arrows: false
-    });
-
-    //toggle scroll menu
-    var scrollTop = 0;
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        //adjust menu background
-        if (scroll > 80) {
-            if (scroll > scrollTop) {
-                $('.smart-scroll').addClass('scrolling').removeClass('up');
-            } else {
-                $('.smart-scroll').addClass('up');
-            }
-        } else {
-            // remove if scroll = scrollTop
-            $('.smart-scroll').removeClass('scrolling').removeClass('up');
-        }
-
-        scrollTop = scroll;
-
-        // adjust scroll to top
-        if (scroll >= 600) {
-            $('.scroll-top').addClass('active');
-        } else {
-            $('.scroll-top').removeClass('active');
-        }
-        return false;
-    });
-
-    // scroll top top
-    $('.scroll-top').click(function () {
-        $('html, body').stop().animate({
-            scrollTop: 0
-        }, 1000);
-    });
-
-    /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
-    });
+    
+	}, 1000);    
+  
 });
